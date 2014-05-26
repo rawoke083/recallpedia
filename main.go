b3c42a76b2e86c69bdb72875507a5651df2f1e40
jacques <=>
2014-05-26 22:32:42 +0200

    wip

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
-#initial
+// Gorilla Mux
+ 
+/*package main
+ 
+import (
+	"github.com/gorilla/mux"
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+)*/
+
+package main
+
+import (
+  "net/http"
+  "log"
+)
+
+func main() {
+  log.Println("Starting Server")
+  http.Handle("/", http.FileServer(http.Dir("./public/")))
+
+  log.Println("Listening on 8080")
+  http.ListenAndServe(":8080", nil)
+}
